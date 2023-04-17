f92f5512ea1afaa1566dd324c149b7ecff248a70
l7g9 <lukewgregory@gmail.com>
2023-04-17 22:01:09 +0100

    add resource block for security group for load balancer

--- main.tf
+++ main.tf
@@ -21,6 +21,36 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "stateless-webapp-vpc" {
+resource "aws_vpc" "stateless_webapp_vpc" {
     cidr_block = "10.0.0.0/16"
 }
+
+# Create security group letting in http traffic from anywhere
+resource "aws_security_group" "allow_http" {
+    name = "allow_http"
+    description = "Allow HTTP inbound traffic"
+    vpc_id = aws_vpc.stateless_webapp_vpc
+
+    ingress {
+        description = "HTTP from anywhere"
+        from_port = 80
+        to_port = 80
+        protocol = "http"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+}
+
+# Create an application load balancer
+#resource "aws_alb" "name" {
+
+#}
+
+# Create an auto scalling group
