245b17a8e89d3724169f542e832234c7dc655ce8
l7g9 <lukewgregory@gmail.com>
2023-04-17 22:39:31 +0100

    add vpc & sg resource blocks

--- main.tf
+++ main.tf
@@ -1,51 +1,51 @@
 terraform {
-    cloud {
-        organization = "LukeGregory-dev"
-        workspaces {
-            name = "aws-stateless-webapp"
-        }
+  cloud {
+    organization = "LukeGregory-dev"
+    workspaces {
+      name = "aws-stateless-webapp"
     }
+  }
 
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 4.16"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
+  }
 
-    required_version = ">= 1.2.0"
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-    region = "eu-west-2"
+  region = "eu-west-2"
 }
 
 # Create a VPC
 resource "aws_vpc" "stateless_webapp_vpc" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "stateless_webapp_vpc"
+  }
 }
 
 # Create security group letting in http traffic from anywhere
 resource "aws_security_group" "allow_http" {
-    name = "allow_http"
-    description = "Allow HTTP inbound traffic"
-    vpc_id = aws_vpc.stateless_webapp_vpc
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.stateless_webapp_vpc.id
 
-    ingress {
-        description = "HTTP from anywhere"
-        from_port = 80
-        to_port = 80
-        protocol = "http"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
+  tags = {
+    Name = "stateless_webapp_sg_allow_http"
+  }
 }
 
 # Create an application load balancer
