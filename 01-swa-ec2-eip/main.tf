43c2cd4c2039c31f8d16c3eb2b6f616cef4154cd
l7g9 <lukewgregory@gmail.com>
2023-04-19 12:21:31 +0100

    add datasource for instance ami

--- 01-swa-ec2-eip/main.tf
+++ 01-swa-ec2-eip/main.tf
@@ -20,9 +20,37 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+# get name using cli
+# aws ec2 describe-images --region eu-west-2 --image-ids ami-0cd8ad123effa531a
+# al2023-ami-2023.0.20230329.0-kernel-6.1-x86_64
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 resource "aws_instance" "web_server" {
-  ami           = "ami-0cd8ad123effa531a"
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
 
   user_data = file("${path.module}/files/user-data.sh")
