80bdeee69e7c5dafe7a196454525404d198bbaf6
l7g9 <lukewgregory@gmail.com>
2023-04-19 14:41:48 +0100

    add security group

--- 01-swa-ec2-eip/main.tf
+++ 01-swa-ec2-eip/main.tf
@@ -48,10 +48,66 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+# create a vpc
+
+# create a security group
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_security_group
+resource "aws_security_group" "web_server" {
+  name        = "web-server"
+  description = "Allow ssh, http & https inbound traffic and all outbound traffic"
+  #vpc_id = optional, default vpc if omitted 
+
+  tags = {
+    Name = "allow-ssh-http-https"
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_security_group_ingress_rule
+resource "aws_vpc_security_group_ingress_rule" "allow_shh" {
+  security_group_id = aws_security_group.web_server.id
+
+  description = "allow incomming ssh from anywhere"
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.web_server.id
+
+  description = "allow incomming http from anywhere"
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.web_server.id
+
+  description = "allow incomming https from anywhere"
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_security_group_egress_rule
+resource "aws_vpc_security_group_egress_rule" "allow_all" {
+  security_group_id = aws_security_group.web_server.id
+
+  description = "allow all outgoing to anywhere"
+  cidr_ipv4   = "0.0.0.0/0"
+
+  ip_protocol = "-1"
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 resource "aws_instance" "web_server" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web_server.id]
 
   user_data = file("${path.module}/files/user-data.sh")
 
@@ -60,7 +116,7 @@ resource "aws_instance" "web_server" {
   }
 }
 
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
 resource "aws_eip" "web_server" {
   instance = aws_instance.web_server.id
   vpc      = true
