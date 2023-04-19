e6074875a297f3f958180176dfed88e865114b89
l7g9 <lukewgregory@gmail.com>
2023-04-19 09:24:10 +0100

    add deployment of stateless web app using ec2 & eip

---
+++ 01-swa-ec2-eip/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  cloud {
+    organization = "LukeGregory-dev"
+    workspaces {
+      name = "swa-01"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+resource "aws_instance" "web_server" {
+  image_id        = "ami-0cd8ad123effa531a"
+  instance_type   = "t2.micro"
+
+  user_data = file("../files/user-data.sh")
+
+  tags = {
+    Name = "swa-01-web-server"
+  }
+}
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
+resource "aws_eip" "web_server" {
+  instance = aws_instance.web_server.id
+  vpc = true
+
+  tags = {
+    Name = "swa-01-web-server-ip"
+  }
+}
