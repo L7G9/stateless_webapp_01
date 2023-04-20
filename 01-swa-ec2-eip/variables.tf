7ad2a40c14aaec720cf221dc67538650b4388dc5
l7g9 <lukewgregory@gmail.com>
2023-04-20 10:41:47 +0100

    add variables.tf file

---
+++ 01-swa-ec2-eip/variables.tf
@@ -0,0 +1,37 @@
+variable "organization" {
+  type = string
+  description = "Terraform Cloud organization defined in main.tf"
+}
+
+variable "workspace" {
+  type = string
+  description = "Terreform Cloud workspace defined in main.tf"
+}
+
+variable "region" {
+  type = string
+  description = "AWS region defined in main.tf"
+}
+
+variable "vpc_cidr" {
+  type = string
+  description = "VPC cidr block defined in network.tf"
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  type = string
+  description = "subnet cidr block defined in netowrk.tf"
+  default = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  type = string
+  description = "Instance type for web app server defined in instance.tf"
+  default = "t2.micro"
+}
+
+variable "user_data_file" {
+  type = string
+  description = "File path for user data of web app server defined in instance.tf"
+}
