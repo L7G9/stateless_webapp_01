1a346781ed68d919c7b7344ab5cb8d70e2dd0f37
l7g9 <lukewgregory@gmail.com>
2023-04-20 09:26:44 +0100

    add security group resource

---
+++ 01-swa-ec2-eip/security.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "this" {
+  name        = "this"
+  description = "Allow ssh, http & https inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.web_server.id
+
+  tags = {
+    Name = "swa_01_sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_shh" {
+  security_group_id = aws_security_group.this.id
+
+  description = "allow incomming ssh from anywhere"
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.this.id
+
+  description = "allow incomming http from anywhere"
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.this.id
+
+  description = "allow incomming https from anywhere"
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all" {
+  security_group_id = aws_security_group.this.id
+
+  description = "allow all outgoing to anywhere"
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
